d29c64fc540f14533b72d967c9b367c0357751dd
ErikaFlores <eflores@dumaxst.com>
2024-05-05 06:13:05 -0600

    feat | Add icon in navbar

--- app/components/ui/navbar/Navbar.tsx
+++ app/components/ui/navbar/Navbar.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+
 import { Dropdown } from "@/app/components/ui";
 
 import styles from './navbar.module.css';
 
 export const Navbar = () => {
   return (
-    <div className={`flex p-8 items-center justify-between text-white ${styles.navbar}`}>
+    <div className={`flex p-4 items-center justify-between text-white ${styles.navbar}`}>
+        <div className='flex items-center gap-2 '>
+          <Image 
+            src="/images/logo-white.png" 
+            width={100}
+            height={25}
+            alt="logo" 
+          />
+          <div className='h-10 border-l-2 border-primary'></div>
 
-
-        <div>
-          <h1>Total Seal</h1>
+          <h1>Total Seal</h1> 
         </div>
         
         <Dropdown/>
-        
-
-
-       
-      
-
-
     </div>
-   
   )
 }
