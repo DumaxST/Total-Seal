1ede4d355d76e49f6c3e799c7d49a898c3fa0b50
ErikaFlores <eflores@dumaxst.com>
2024-05-06 13:32:26 -0600

    feat| Add HorizontalBar chart to dashboard/main

--- app/components/ui/horizontalBar/HorizontalBar.tsx
+++ app/components/ui/horizontalBar/HorizontalBar.tsx
@@ -12,21 +12,17 @@ export const HorizontalBar = ()=> {
         const textColor = documentStyle.getPropertyValue('--text-color');
         const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
         const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
         const data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: ['Unidad Grupo','Unidad Grupo', 'Unidad Grupo', 'Unidad Grupo', 'Unidad Grupo'],
             datasets: [
                 {
-                    label: 'My First dataset',
-                    backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-                    borderColor: documentStyle.getPropertyValue('--blue-500'),
-                    data: [65, 59, 80, 81, 56, 55, 40]
+                    label: 'Alertas',
+                    backgroundColor: documentStyle.getPropertyValue('--secondary-200'),
+                    borderColor: documentStyle.getPropertyValue('--secondary-200'),
+                    data: [14, 11, 11, 7, 6]
                 },
-                {
-                    label: 'My Second dataset',
-                    backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-                    borderColor: documentStyle.getPropertyValue('--pink-500'),
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
+               
             ]
         };
         const options = {
