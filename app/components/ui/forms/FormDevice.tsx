d9b52c6ae8115ca83c2b239417fbc815e911dda7
ErikaFlores <eflores@dumaxst.com>
2024-05-09 14:51:15 -0600

    refactor | Refacor component Dropdown

--- app/components/ui/forms/FormDevice.tsx
+++ app/components/ui/forms/FormDevice.tsx
@@ -2,14 +2,13 @@
 import React, {useState} from 'react';
 import { MultiSelect } from 'primereact/multiselect';
 import { Button } from '@/app/components/ui';
-import {devicesAll } from "@/app/lib/data";
 import {DeviceDropdown} from "@/app/lib/definitions";
 interface FormDeviceProps {
     data: DeviceDropdown[]
 }
 export const FormDevice = ({data }: FormDeviceProps) => {
-    
-    const [selectedCities, setSelectedCities] = useState(null);
+
+    const [selectedData, setSelectedData] = useState(null);
     
   return (
     <form action="" className='flex flex-row justify-between items-center'>
@@ -18,8 +17,8 @@ export const FormDevice = ({data }: FormDeviceProps) => {
             <label htmlFor="selectedDevice">Ver todas las unidades</label>
             <MultiSelect 
                 id="selectedDevice"
-                value={selectedCities} 
-                onChange={(e) => setSelectedCities(e.value)} 
+                value={selectedData} 
+                onChange={(e) => setSelectedData(e.value)} 
                 options={data} 
                 optionLabel="name"
                 display="chip" 
