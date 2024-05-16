f698f5ff9150be8ddeed9075e83c602cc8a8db53
ErikaFlores <eflores@dumaxst.com>
2024-05-16 13:58:52 -0600

    styles | Refactor Avatar section, and update in movement icon

--- app/components/ui/button/Button.tsx
+++ app/components/ui/button/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 }
 export const Button = ({text}:ButtonProps) => {
   return (
-    <button type="button" className={`max-h-8 rounded	 text-white bg-secondary font-bold  py-2 px-6  focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50  text-sm  text-center inline-flex items-center `}>
+    <button type="button" className={`max-h-8 rounded	 text-white bg-secondary font-bold  py-2 px-6  focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50  text-xs  text-center inline-flex items-center `}>
       {text}     
     </button>
   )
