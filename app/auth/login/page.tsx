4cd70e547971af4ca9c7b5740c4d5b8294d8e096
ErikaFlores <eflores@dumaxst.com>
2024-05-19 22:30:13 -0600

    styles | Update font size title and subtitle, update gap in heroForm

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function LoginPage() {
   return (
     <section className={`heroForm pt-6 pb-6 pr-5 pl-5`}>
-    <h2 className={`${headingFont.className} heroForm__title text-5xl `}>Bienvenido a Total Seal </h2>
-    <h3 className={`${headingFont.className} heroForm__subtitle text-3xl uppercase`}>Iniciar sesión</h3>
+    <h3 className={`${headingFont.className} heroForm__title text-3xl`}>Bienvenido</h3>
+    <h3 className={`${headingFont.className} heroForm__subtitle text-2xl uppercase`}>Iniciar sesión</h3>
     <form className="form">
       
       <div className="form__group">
