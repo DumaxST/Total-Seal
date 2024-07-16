073b24760e2fea85b5f829739223b68221138469
Daniel Guzman <nylecks@hotmail.com>
2024-07-15 19:07:36 -0600

    ok

--- app/components/ui/forms/FormDevice.tsx
+++ app/components/ui/forms/FormDevice.tsx
@@ -1,35 +1,33 @@
 'use client';
-import React, {useState} from 'react';
-import { MultiSelect } from 'primereact/multiselect';
+import React, { useState } from 'react';
+import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect';
 import { Button } from '@/app/components/ui';
-import {DeviceDropdown} from "@/app/lib/definitions";
-interface FormDeviceProps {
-    data: DeviceDropdown[]
-}
-export const FormDevice = ({data }: FormDeviceProps) => {
+import { DeviceDropdown } from "@/app/lib/definitions";
 
-    const [selectedData, setSelectedData] = useState(null);
-    
+interface FormDeviceProps {
+  data: DeviceDropdown[]
+}
+
+export const FormDevice = ({ data }: FormDeviceProps) => {
+  const [selectedData, setSelectedData] = useState<DeviceDropdown[] | null>(null);
+
   return (
     <form action="" className='flex flex-row justify-between items-center'>
-        
-        <div className="flex flex-col w-60">
-            <label htmlFor="selectedDevice">Ver todas las unidades</label>
-            <MultiSelect 
-                id="selectedDevice"
-                value={selectedData} 
-                onChange={(e) => setSelectedData(e.value)} 
-                options={data} 
-                optionLabel="name"
-                display="chip" 
-                maxSelectedLabels={3} 
-                className='heading-h3'
-                filter
-            />
-
-        </div>
-              
-        <Button text='Generar reporte'/>
+      <div className="flex flex-col w-60">
+        <label htmlFor="selectedDevice">Ver todas las unidades</label>
+        <MultiSelect
+          id="selectedDevice"
+          value={selectedData}
+          onChange={(e: MultiSelectChangeEvent) => setSelectedData(e.value)}
+          options={data}
+          optionLabel="name"
+          display="chip"
+          maxSelectedLabels={3}
+          className='heading-h3'
+          filter
+        />
+      </div>
+      <Button text='Generar reporte' />
     </form>
-  )
+  );
 }
