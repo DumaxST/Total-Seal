1dd9d4f7bf6b06fbc3d77060829269a6f1b1b775
ErikaFlores <eflores@dumaxst.com>
2024-07-16 21:54:08 -0600

    fix | fix build bug route.ts is not a module

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,64 +1,26 @@
+
 import NextAuth from "next-auth"
-import type { NextAuthOptions } from "next-auth";
-
-
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-  session:{
-    strategy: 'jwt',
-  },
-  pages:{
-    signIn: '/auth/login',
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        username: {},
-        password: {}
-      },
-      async authorize(credentials, req) {
-      
-        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/login`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            username: credentials?.username,
-            password: credentials?.password
-          })
-        })
-        
-        const data = await response.json();
-        if(data){
-          return {
-            id: data?.id,
-            name: data?.name,
-            email: data?.email
-          }
-        }
-        return null
-      }
-    })
+const authOptions: NextAuthOptions = {
+    session: {
+      strategy: "jwt",
+    },
+    pages: {
+      signIn: "/auth/login",
+    },
+    providers: [
+      CredentialsProvider({
+        credentials: {
+          username: {},
+          password: {},
+        },
+        async authorize(credentials, req) {
+          return null;
+        },
+      }),
     ],
-    callbacks :{
-      async signIn({user, account}){
-       
-        return true
-      },
-      async jwt({token}){
-        console.log("token")
-        console.log({token})
-        return token
-      },
-      async session({session, token}){
-        console.log("session")
-        console.log({session})
-        return session;
-      }
-    }
-  
-  }
+  };
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST } 
