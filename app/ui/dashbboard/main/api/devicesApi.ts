6d947ddcfd001d807f6f60a80de0f3b9efc627a9
ErikaFlores <eflores@dumaxst.com>
2024-07-18 05:35:17 -0600

    refactor | Save as en variable the api endpoint

--- app/ui/dashbboard/main/api/devicesApi.ts
+++ app/ui/dashbboard/main/api/devicesApi.ts
@@ -2,7 +2,8 @@ import { getSession } from 'next-auth/react';
 
 const URLS = {
     SEAL_DEVICES :'seal_devices',
-    SEAL_SETTINGS: 'users/settings'
+    SEAL_SETTINGS: 'users/settings',
+    LAST_CONNECTION: 'devices/2/list'
 }
 
 export async function fetchDevices(token: string) {
@@ -19,22 +20,8 @@ export async function fetchDevices(token: string) {
     
     return data.seal_devices
 }
-export async function fetchCode(){
-    const session = await getSession();
-    console.log(session)
-    const response =  await fetch("https://lite.dumaxst.com/v1/users/settings", {
-        method: 'GET',
-        headers: {
-          'X-Api-Key': "KFQ6sVywS7dG2in8FUEy27dRu3AYmlqR/HgpUOgVAVA=",
-          'Content-Type': 'application/json',
-          'Uuid': 'RESTFul-API',
-          "App": "RESTFul API"
-        }
-      });
-        const data = await response.json();    
-}
 export async function getLastConnection(token:string,imei: string){
-    const response =  await fetch(`https://lite.dumaxst.com:5000/v1/devices/2/list`, {
+    const response =  await fetch(`${process.env.NEXT_PUBLIC_MAPI_SG_URL}/${URLS.LAST_CONNECTION}`, {
         method: 'POST',
         headers: {
           'X-Api-Key': token,
