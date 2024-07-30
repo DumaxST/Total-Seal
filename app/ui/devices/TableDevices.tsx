69c3f4f6ed97f4926d7c5570ab124cdfa09018e9
ErikaFlores <eflores@dumaxst.com>
2024-07-30 05:31:07 -0600

    styles | Update styles table in devices page

--- app/ui/devices/TableDevices.tsx
+++ app/ui/devices/TableDevices.tsx
@@ -2,14 +2,18 @@
 import {useState} from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-type Priority = "low | high";
+import { bodyFont } from "@/app/config/fonts";
+import { Tag } from "primereact/tag";
+import Link from "next/link";
+type Priority = 'low'|'high';
 
-interface Alert{
+export interface Alert{
     id:string
     device:string;
+    idDevice:string;
     createdAt: string;
     codeSeal: string;
-    priority?: Priority;
+    priority: Priority;
     compartment: string;
     event: string;
 }
@@ -20,12 +24,40 @@ export const TableDevices = ({alerts}:Props) => {
   
   const [alertsList, setAlertsList] = useState<Alert[]>(alerts);
 
+  const priorityLabel ={
+    low: "Baja",
+    high: "Alta"
+  };
+
+  const deviceBodyTemplate = (rowData:Alert) => (
+    <span  className={`${bodyFont.className} text-xs  `}>{rowData.device}</span>
+  );
+  const dateBodyTemplate = (rowData:Alert) =>(
+    <span  className={`${bodyFont.className} text-xs  `}>{rowData.createdAt}</span>
+  );
+  const priorityBodyTemplate = (rowData:Alert) => (
+    <Tag  className={`${bodyFont.className} text-xs`} severity={rowData.priority === 'low'?  "success" : "danger"} value={priorityLabel[rowData.priority]}/>
+  )
+  const codeSealBodyTemplate = (rowData:Alert) => (
+    <span  className={`${bodyFont.className} text-xs  `}>{rowData.codeSeal}</span>
+  )
+
+  const actionBodyTemplate = (rowData:Alert) => (
+    <Link href={`/device/${rowData.idDevice}`} key={`/device`} >
+      <button type="button" className={`${bodyFont.className} pt-4 pb-4 pl-4  max-h-8 rounded	 text-white bg-secondary font-bold  py-2 px-6  focus:outline-none  text-xs  text-center flex items-center flex-row justify-center `}>
+          Ver actividad
+      </button>
+    </Link>
+  )
   return (
-    <DataTable value={alertsList} stripedRows>
-                <Column field="device" header="Unidad"></Column>
-                <Column field="createdAt" header="Fecha"></Column>
-                <Column field="priority" header="Prioridad"></Column>
-                <Column field="codeSeal" header="Código de Sello"></Column>
+    <DataTable value={alertsList} stripedRows size="small" tableStyle={{ minWidth: "45rem" }}
+    emptyMessage="Sin alertas registradas">
+                <Column field="device" sortable body={deviceBodyTemplate} header={"Unidad"} headerClassName={`${bodyFont.className}  text-xs rounded-tl pl-4 text-left `}/>
+                <Column field="createdAt" sortable header="Fecha" body={dateBodyTemplate} headerClassName={`${bodyFont.className}  text-xs text-left `}/>
+                <Column field="priority" sortable header="Prioridad" body ={priorityBodyTemplate} headerClassName={`${bodyFont.className}  text-xs text-left `}/>
+                <Column field="codeSeal" sortable header="Código de Sello"  body ={codeSealBodyTemplate}  headerClassName={`${bodyFont.className}  text-xs pl-4 text-left `}/>
+                <Column field="codeSeal"  header="Acciones" body ={actionBodyTemplate} headerClassName={`${bodyFont.className}  text-xs rounded-tr  pr-4 text-left `}/>
+
     </DataTable>
   )
 }
